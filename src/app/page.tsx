4916ea73a0ce33c7736d153f307e4052c34e03c0
Pierre GAMEN <pierre.gamen@gmail.com>
2024-10-04 09:17:21 +0200

    feat : menu to chose a game

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,40 @@
 
 import Welcome from "@/component/Welcome/Welcome";
 import AnimateTitle from "@/component/AnimateTitle/AnimateTitle";
+import RightPrice from "@/component/RightPrice";
 import styles from "./page.module.css";
 import { useState } from "react";
-import "./page.scss"
+import "./page.scss";
 
 export default function Home() {
   const [welcomePage, setWelcomePage] = useState(true);
+  const [chosenGame, setChosenGame] = useState("");
 
   const handleCloseWelcome = () => {
-    setWelcomePage(false);
+    if (localStorage.getItem("name")) {
+      setWelcomePage(false);
+    } else return;
+  };
+
+  const chooseAGame = (e: React.MouseEvent<HTMLLIElement, MouseEvent>) => {
+    setChosenGame(e.currentTarget.innerText);
   };
 
   return (
     <div className="page">
       <main className={styles.main}>
-        <AnimateTitle />
+        {!chosenGame && <AnimateTitle />}
         {welcomePage && <Welcome handleCloseWelcome={handleCloseWelcome} />}
+        {(!welcomePage && !chosenGame) && (
+          <div>
+            <li onClick={chooseAGame}>Tic Tac Toe</li>
+            <li onClick={chooseAGame}>Pierre feuille ciseaux</li>
+            <li onClick={chooseAGame}>Le juste prix</li>
+          </div>
+        )}
+        {/* {chosenGame === "Tic Tac Toe" && <TicTacToe />} */}
+        {/* {chosenGame === "Pierre feuille ciseaux" && <PFC />} */}
+        {chosenGame === "Le juste prix" && <RightPrice />}
       </main>
     </div>
   );
