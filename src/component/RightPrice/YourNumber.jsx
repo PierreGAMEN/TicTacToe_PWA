bda2b868f66edac3cbaad95e49ee659d2d635b91
Pierre GAMEN <pierre.gamen@gmail.com>
2024-10-05 16:50:26 +0200

    style

--- src/component/RightPrice/YourNumber.jsx
+++ src/component/RightPrice/YourNumber.jsx
@@ -4,95 +4,120 @@ const YourNumber = () => {
   const [minNumber, setMinNumber] = useState(0);
   const [maxNumber, setMaxNumber] = useState(100);
   const [answer, setAnswer] = useState(0);
-  const [win, setWin] = useState(false)
-  const [numberOfTry, SetnumberOfTry] = useState(0)
+  const [win, setWin] = useState(false);
+  const [numberOfTry, SetnumberOfTry] = useState(0);
 
   const getFirstNumber = () => {
     setAnswer(Math.floor((maxNumber + minNumber) / 2));
-    SetnumberOfTry(numberOfTry + 1)
+    SetnumberOfTry(numberOfTry + 1);
   };
 
   const getMinimum = (e) => {
-    const value = Number(e.target.value); 
-    setMinNumber(value < 0 ? 0 : value); 
+    const value = Number(e.target.value);
+    setMinNumber(value < 0 ? 0 : value);
   };
 
   const getMaximum = (e) => {
-    const value = Number(e.target.value); 
+    const value = Number(e.target.value);
     setMaxNumber(value);
   };
 
   const changeMinAndGiveAnAnswer = () => {
-    SetnumberOfTry(numberOfTry + 1)
-    setMinNumber(answer); 
+    SetnumberOfTry(numberOfTry + 1);
+    setMinNumber(answer);
     const diff = maxNumber - answer;
     if (diff > 2) {
-      setAnswer(answer + Math.round((diff/2)));
+      setAnswer(answer + Math.round(diff / 2));
+    } else {
+      setAnswer(answer + 1);
     }
-    else {
-      setAnswer(answer + 1)
-    }
-}
+  };
 
   const changeMaxAndGiveAnAnswer = () => {
-    SetnumberOfTry(numberOfTry + 1)
-    setMaxNumber(answer); 
+    SetnumberOfTry(numberOfTry + 1);
+    setMaxNumber(answer);
     const diff = answer - minNumber;
     if (diff > 2) {
-      setAnswer(answer - Math.round((diff/2)));
+      setAnswer(answer - Math.round(diff / 2));
+    } else {
+      setAnswer(answer - 1);
     }
-    else {
-      setAnswer(answer - 1)
-    }
-  }
+  };
 
   const reMatch = () => {
-    setMinNumber(0)
-    setMaxNumber(100)
-    setAnswer(0)
-    setWin(false)
-    SetnumberOfTry(0)
-  }
-
-
+    setMinNumber(0);
+    setMaxNumber(100);
+    setAnswer(0);
+    setWin(false);
+    SetnumberOfTry(0);
+  };
 
   return (
     <section className="YourNumber">
-  
-      {!answer && <>
-      <p>Veuillez choisir la plage de jeu </p>
-      <div className="container_input">
-        <label htmlFor="min">Minimum</label>
-        <input value={minNumber} onChange={getMinimum} type="number" id="min" />
-      </div>
-      <div className="container_input">
-        <label htmlFor="max">Maximum</label>
-        <input value={maxNumber} onChange={getMaximum} type="number" id="max" />
-      </div>
-      <button onClick={getFirstNumber}>Commencer</button>
-      {maxNumber <= minNumber && (
-        <p>
-          Attention le nombre maximal est inférieur ou égal au nombre minimal
-        </p>
+      {/* PARTIE 1  */}
+      {!answer && (
+        <>
+          <p className="rule">Dans ce mode de jeu, vous devez choisir une plage de jeu et dans cette plage choisir un nombre dans votre tête que Party_Robot devra trouver le plus rapidement possible.</p>
+          <div className="container_input">
+            <p>Plage</p>
+            <input
+              value={minNumber}
+              onChange={getMinimum}
+              type="number"
+              id="min"
+            />
+            <input
+              value={maxNumber}
+              onChange={getMaximum}
+              type="number"
+              id="max"
+            />
+          </div>
+          <button onClick={getFirstNumber}>Commencer</button>
+          {maxNumber <= minNumber && (
+            <p>
+              Attention le nombre maximal est inférieur ou égal au nombre
+              minimal
+            </p>
+          )}
+        </>
       )}
-      </>}
-      {!win && <>
-      {answer > 0 && <div className="container_button">
-        <button onClick={changeMinAndGiveAnAnswer}>C&apos;est plus</button>
-        <button onClick={changeMaxAndGiveAnAnswer}>C&apos;est moins</button>
-        <button onClick={() => {setWin(true)}}>C&apos;est exact</button>
-      </div>}
-      <div className="container_robot">
-      {answer > 0 && <p className="answer">{answer}</p>}
-      {answer > 0 && <img className="robot" src="robot0.svg"></img>}
-      </div>
-      
-      {answer > 0 && <div>Tentative: {numberOfTry}</div>}
-      </>}
-      {win && <div>
-        <p>Party_Robot a encore gagné !</p>
-        <button onClick={reMatch}>Rejouer</button>
-      </div>}
+
+      {/* PARTIE 2  */}
+
+      {!win && (
+        <>
+          {answer > 0 && (
+            <div className="container_button">
+              <button onClick={changeMinAndGiveAnAnswer}>
+                C&apos;est plus
+              </button>
+              <button onClick={changeMaxAndGiveAnAnswer}>
+                C&apos;est moins
+              </button>
+              <button
+                onClick={() => {
+                  setWin(true);
+                }}
+              >
+                C&apos;est exact
+              </button>
+            </div>
+          )}
+          <div className="container_robot">
+            {answer > 0 && <p className="answer">{answer}</p>}
+            {answer > 0 && <img className="robot" src="robot0.svg"></img>}
+          </div>
+
+          {answer > 0 && <div>Tentative: {numberOfTry}</div>}
+        </>
+      )}
+      {win && (
+        <div>
+          <p>Party_Robot a encore gagné !</p>
+          <button onClick={reMatch}>Rejouer</button>
+        </div>
+      )}
     </section>
   );
 };
